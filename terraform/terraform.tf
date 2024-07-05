18986ed7aeb6092160ca952e36f47e646008acf4
Mayankpatil-k8s <mayank.devops.k8s@gmail.com>
2024-07-05 20:46:49 +0530

    deploy jumphost

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
     }
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "devsecops-project1"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
